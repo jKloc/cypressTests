a6f6721e184f0966c6c9cf79fe422673874dc000
Marcin Kloc <marcin.k.kloc@gmail.com>
2019-10-28 17:54:41 +0100

    Added config file, cookie management

---
+++ cypress/config/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    "baseUrl": "http://localhost:8001",
+    "username": "#####",
+    "password": "########"
+}
--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -1,25 +1,64 @@
-/// <reference types="cypress"/>
-
-/*context("test google page", () => {
-    it("Search google for trials bike", () => {
-        cy.visit('https://www.google.pl')
-        .get('input[type="text"]')
-        .type('trial bike')
-        .get("form")
-        .submit()
-        .url()
-        .should('include', 'q=');
-    });
-})
-*/
-
-context("test google page", () => {
-    it("Search google for trials bike", () => {
-        cy.visit('https://www.gsmmaniak.pl')
-        //.get('form')
-        .contains('Już od dzisiaj kupisz OnePlusa 7T oraz 7T Pro w Polsce.')
+/// <reference types="Cypress" />
+//import md5 from "md5";
+//import fs from "fs-extra";
+import * as config from '../config/config';
+ 
+describe('Simple test',() => {
+ 
+    before(() => {
+        Cypress.config('baseUrl', config.baseUrl)
+        Cypress.config('pageLoadTimeout', 100000)
+    })
+ 
+    beforeEach(() => {
+        cy.visit('/')
+        Cypress.Cookies.preserveOnce('laravel_session')
+        Cypress.Cookies.preserveOnce('_token')
+        Cypress.Cookies.preserveOnce('currentUser')
+        Cypress.Cookies.preserveOnce('sessionid')
+        Cypress.Cookies.preserveOnce('isSuperAdmin')
+        Cypress.Cookies.preserveOnce('isOrganizationAdmin')
+    })
+ 
+    afterEach(() => {
+        //cacheLocalStorageAuth()
+        //cy.saveLocalStorage();
+    })
+ 
+    it('log in',() => {
+        //this.skip();
+ 
+        cy.visit('/login')
+        .get('#user-name')
+        .type(config.username)
+ 
+        cy.get('#password')
+        .type(config.password, { log: false })
+ 
+        cy.get('button[type=submit]')
         .click()
-        .url()
-        .should('eq', 'https://www.gsmmaniak.pl/1059558/oneplus-7t-polska/#pid=wd')
-    });
+ 
+        cy.get('button')
+        .should('contain', 'Admin')
+        .visible
+    })
+ 
+    it('log out', function () {
+        this.skip();
+        cy.get('button')
+        .should('contain', 'Wyloguj')
+        .click()
+    })
+ 
+    it('Simple test datasets', function () {
+        //this.skip();
+        cy.contains('Zbiory')
+        .click()
+
+        cy.get('a[href*=datasets]')
+        .click
+ 
+        // cy.get('a[href*=login]').click()
+        // cy.get('button[type="submit"]').click()
+    })
 })
